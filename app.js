f869afa589d704f516c60f4a96c267c9a7867a5b
Jack Lewis <jack.lewis159@gmail.com>
2024-02-13 18:04:16 +0000

    Altered routing and successfully performed testing for requesting server-side code to perform DB query from client-side through POST request

--- app.js
+++ app.js
@@ -20,10 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/bct-wrapped', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bct-wrapped', playersRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,51 +38,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-
-
-//BCT Stats ASCII Text
-console.log(`\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\r\n\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D    \u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2551        \u2588\u2588\u2551       \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\r\n\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551        \u2588\u2588\u2551       \u255A\u2550\u2550\u2550\u2550\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551   \u2588\u2588\u2551   \u255A\u2550\u2550\u2550\u2550\u2588\u2588\u2551\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u255A\u2588\u2588\u2588\u2588\u2588\u2588\u2557   \u2588\u2588\u2551       \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551  \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\r\n\u255A\u2550\u2550\u2550\u2550\u2550\u255D  \u255A\u2550\u2550\u2550\u2550\u2550\u255D   \u255A\u2550\u255D       \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D   \u255A\u2550\u255D   \u255A\u2550\u255D  \u255A\u2550\u255D   \u255A\u2550\u255D   \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\r\n                                                                       \r\n`)
-
-// MySQL database configuration
-const mysql = require('mysql');
-const dbHost = "193.70.81.206";
-const dbUsername = "bailey";
-const dbPassword = "xjeEw52LcUFSi3";
-const dbDatabase = "bct-stats";
-
-const dbConnection = mysql.createConnection({
-    host: dbHost,
-    user: dbUsername,
-    password: dbPassword,
-    database: dbDatabase
-});
-
-app.set('dbConnection', dbConnection);
-
-// Connect to the database
-dbConnection.connect((err) => {
-    if (err) {
-        console.error('Failed to connect to BCT Stats database: ', err);
-        return;
-    }
-    console.log.info('Connected to BCT Stats database!');
-});
-
-// Route to handle requests from frontend
-app.post('/bct-wrapped/getAllPlayers', (req, res) => {
-  console.log("Making query to bct-stats DB");
-    const query = "CALL GetAllPlayers()"; // Stored procedure
-    dbConnection.query(query, (err, result) => {
-        if (err) {
-            console.error('Database Error:', err);
-            res.status(500).json({ error: 'Database Error' });
-            return;
-        }
-        console.log(result);
-        res.json(result);
-    });
 });
 
 module.exports = app;
