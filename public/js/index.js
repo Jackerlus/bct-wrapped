05aabde536229e52f4f6a5b5d996dc15d3bf7929
Jack Lewis <jack.lewis159@gmail.com>
2024-02-13 18:04:16 +0000

    Altered routing and successfully performed testing for requesting server-side code to perform DB query from client-side through POST request

--- public/js/index.js
+++ public/js/index.js
@@ -2,21 +2,22 @@ $(window).on('load', function() {
     $("#loader-wrapper").fadeOut(700);
 });
 
-// Example using fetch API
-fetch('http://jacklewis.me/bct-wrapped/getAllPlayers', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ /* if you need to pass any data */ })
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data); // Handle the response data here
-    for (let player of data[0]) {
-        $('#list-players').append(`<li>${player.username}</li>`);
-    }
-})
-.catch(error => {
-    console.error('Error:', error);
+$('#button-submit').click(function() {
+    const discord = $('#input-discord').val();
+    console.log("Discord user received: " + discord)
+    fetch('/bct-wrapped/getPlayerStats', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ discord: discord })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Handle the response data here
+        $('#list-players').append(`<li>Data received</li>`);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 });
--- routes/players.js
+++ routes/players.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
+//BCT Stats ASCII Text
+console.log(`\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\r\n\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D    \u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2551        \u2588\u2588\u2551       \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\r\n\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551        \u2588\u2588\u2551       \u255A\u2550\u2550\u2550\u2550\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551   \u2588\u2588\u2551   \u255A\u2550\u2550\u2550\u2550\u2588\u2588\u2551\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u255A\u2588\u2588\u2588\u2588\u2588\u2588\u2557   \u2588\u2588\u2551       \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551  \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\r\n\u255A\u2550\u2550\u2550\u2550\u2550\u255D  \u255A\u2550\u2550\u2550\u2550\u2550\u255D   \u255A\u2550\u255D       \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D   \u255A\u2550\u255D   \u255A\u2550\u255D  \u255A\u2550\u255D   \u255A\u2550\u255D   \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\r\n                                                                       \r\n`)
+
+// MySQL database configuration
+const mysql = require('mysql');
+const dbHost = process.env.DB_HOST;
+const dbUsername = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+const dbDatabase = process.env.DB_NAME;
+
+const dbConnection = mysql.createConnection({
+    host: dbHost,
+    user: dbUsername,
+    password: dbPassword,
+    database: dbDatabase
+});
+
+// Connect to the database
+dbConnection.connect((err) => {
+    if (err) {
+        console.error('Failed to connect to BCT Stats database: ', err);
+        return;
+    }
+    console.log('Connected to BCT Stats database!');
+});
+
+
 // Route to handle requests from frontend
-router.post('/getAllPlayers', (req, res) => {
-    const dbConnection = req.app.get('dbConnection'); // Get the database connection from app.js
-    const query = "CALL GetAllPlayers()"; // Stored procedure
-    dbConnection.query(query, (err, result) => {
+router.post('/GetPlayerStats', (req, res, next) => {
+    const discord = req.body.discord;
+    console.log("Making query to bct-stats DB with player discord: " + discord);
+    const query = "CALL getPlayerWrappedStats(?)"; // Stored procedure
+    dbConnection.query(query, [discord], (err, result) => {
         if (err) {
             console.error('Database Error:', err);
             res.status(500).json({ error: 'Database Error' });
             return;
         }
+        console.log(result);
         res.json(result);
     });
 });
