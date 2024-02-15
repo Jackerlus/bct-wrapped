cbb52681166a6c009de279a1f5d3c508d549e7c6
Jack Lewis <jack.lewis159@gmail.com>
2024-02-15 16:43:47 +0000

    - Reorganised containers so basic player details always stay on top of stats - Added meta theme color data - Added new post and database call for percentile stats for player - Combat stats section filled out with ACS and K/D stats with percentiles - Conditional message depending on player performance added underneath each stat - Modified CSS slightly to increase Combat stats title margin and size

--- public/js/index.js
+++ public/js/index.js
@@ -5,24 +5,36 @@ $(window).on('load', function() {
 $('#button-submit').click(function() {
     const discord = $('#input-discord').val();
     console.log("Discord user received: " + discord)
-    fetch('/bct-wrapped/getPlayerStats', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ discord: discord })
-    })
-    .then(response => response.json())
+    Promise.all([
+        fetch('/bct-wrapped/getMatchStats', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ discord: discord })
+        }),
+        fetch('/bct-wrapped/getPercentileStats', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ discord: discord })
+        }),
+    ])
+    .then(responses => Promise.all(responses.map(response => response.json())))
     .then(data => {
-        console.log(data); // Handle the response data here
+        console.log(data);
 
-        const agent = findMostPlayedAgent(data);
+        const matchData = data[0];
+        const percentileData = data[1];
+
+        const agent = findMostPlayedAgent(matchData);
         console.log("Most played agent: " + agent[0] + " played " + agent[1] + " times.");
         $('#stats-container').empty();
-        generateBasics(data, agent);
+        generateBasics(matchData, agent);
         $('#button-next').click(function() {
-            $('#stats-container').empty();
-            generateCombatStats(data);
+            $('#button-next').remove();
+            generateCombatStats(matchData, percentileData, agent[0]);
         });
     })
     .catch(error => {
@@ -30,10 +42,10 @@ $('#button-submit').click(function() {
     });
 });
 
-function findMostPlayedAgent(data) {
+function findMostPlayedAgent(matchData) {
     const agentCounts = {};
     let mostPlayedAgent = ["", 0];
-    data[0].forEach(match => {
+    matchData[0].forEach(match => {
         const agent = match.agent;
         if (agentCounts[agent]) {
             agentCounts[agent]++;
@@ -52,21 +64,51 @@ function findMostPlayedAgent(data) {
     return mostPlayedAgent;
 }
 
-function generateBasics(data, mostPlayedAgent) {
-    $('#stats-container').append(`
+function generateBasics(matchData, mostPlayedAgent) {
+    $('#basic-details').empty();
+    $('#basic-details').append(`
         <img src="/img/${mostPlayedAgent[0]}_icon.png" class="m-2 max-w-50 border border-2 shadow-lg rounded-lg">
-        <p class="m-1 font-medium">${data[0][0]["player_name"]}</p>
-        <p class="m-1 font-medium">Seed ${data[0][0]["seed"]} &#8226; ${data[0][0]["team_name"]}</p>
+        <p class="m-1 font-medium">${matchData[0][0]["player_name"]}</p>
+        <p class="m-1 font-medium">Seed ${matchData[0][0]["seed"]} &#8226; ${matchData[0][0]["team_name"]}</p>
+        <button id="button-next" class="m-3 p-2 max-w-15 max-h-10 font-base uppercase border-orange-400 active:bg-orange-400 hover:bg-orange-400/80 shadow-lg border rounded-lg transition-all">
+            Next
+        </button>
+    `);
+    $('#basic-details').removeClass('opacity-0 invisible').addClass('opacity-100').hide().fadeIn();
+}
+
+function generateCombatStats(matchData, percentileData, mostPlayedAgentName) {
+    console.log(percentileData);
+
+    let kd_remark;
+    if (percentileData[0][0]["kd"] >= 1) {
+        kd_remark = "check them PC!";
+    } else {
+        kd_remark = "DM senk for kovaak's playlist ;)";
+    }
+
+    let acs_remark;
+    if (percentileData[0][0]["acs_percentile"] >= 50) {
+        acs_remark = "menace on the server";
+    } else {
+        acs_remark = "redemption arc incoming";
+    }
+
+    $('#stats-container').empty();
+    $('#stats-container').append(`
+        <h2 class="m-3 font-medium text-xl">Combat stats:</h2>
+        <p class="m-1 font-medium">Your average K/D is ${percentileData[0][0]["kd"]} - top ${(100 - percentileData[0][0]["kd_percentile"]).toFixed(0)}%</p>
+        <p class="text-sm">${kd_remark}</p>
+        <br>
+        <p class="m-1 font-medium">Your average ACS is ${percentileData[0][0]["acs"]} - top ${(100 - percentileData[0][0]["acs_percentile"]).toFixed(0)}%</p>
+        <p class="text-sm">${acs_remark}</p>
+        <br>
         <button id="button-next" class="m-3 p-2 max-w-15 max-h-10 font-base uppercase border-orange-400 active:bg-orange-400 hover:bg-orange-400/80 shadow-lg border rounded-lg transition-all">
             Next
         </button>
     `);
     $('#stats-container').removeClass('opacity-0 invisible').addClass('opacity-100').hide().fadeIn();
-}
+    $('#button-next').on('click', function() {
 
-function generateCombatStats(data) {
-    $('#stats-container').append(`
-        <h2>Combat stats:</h2>
-        <p class="m-1 font-medium">Your average combat score is ${data[0][0]["seed"]}</p>
-    `);
+    });
 }
